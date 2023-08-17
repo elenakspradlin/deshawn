8876f0eccfc39cfddb40eb02fd01dc212cdf19f9
Elena Spradlin <ekspradlin@gmail.com>
2023-08-17 15:56:10 -0400

    Deshawn appointment table added

--- deshawnapi/views/city_view.py
+++ deshawnapi/views/city_view.py
@@ -10,19 +10,16 @@ class CityView(ViewSet):
 
     def retrieve(self, request, pk=None):
         # Step 1: Get a single city based on the primary key in the request URL
+      # To serialize a single city (1 row)
         city = City.objects.get(pk=pk)
-
-        # Step 2: Serialize the city record as JSON
-        serialized = CitySerializer(city, context={'request': request})
+        serialized = CitySerializer(city, many=False)
 
         # Step 3: Send JSON response to client with 200 status code
         return Response(serialized.data, status=status.HTTP_200_OK)
 
     def list(self, request):
-        # Step 1: Get all city data from the database
+        # To serialize a list of cities (multiple rows)
         cities = City.objects.all()
-
-        # Step 2: Convert the data to JSON format
         serialized = CitySerializer(cities, many=True)
 
         # Step 3: Respond to the client with the JSON data and 200 status code
@@ -34,4 +31,3 @@ class CitySerializer(serializers.ModelSerializer):
     class Meta:
         model = City
         fields = ('id', 'name',)
-
